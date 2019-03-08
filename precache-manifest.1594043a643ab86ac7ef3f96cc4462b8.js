402831880ea46e770f0d6fdb20514da0a7ad2174
lhp0121 <lhp0121@hotmail.com>
2019-03-08 00:02:35 -0500

    Updates

---
+++ precache-manifest.1594043a643ab86ac7ef3f96cc4462b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6b360e82bc180fea2b",
+    "url": "/timeslotdemo/static/js/runtime~main.6f6b360e.js"
+  },
+  {
+    "revision": "c06c8b354784a2b0decb",
+    "url": "/timeslotdemo/static/js/main.c06c8b35.chunk.js"
+  },
+  {
+    "revision": "af2d423fb1801c21fca3",
+    "url": "/timeslotdemo/static/js/2.af2d423f.chunk.js"
+  },
+  {
+    "revision": "c06c8b354784a2b0decb",
+    "url": "/timeslotdemo/static/css/main.dfc13b5d.chunk.css"
+  },
+  {
+    "revision": "af2d423fb1801c21fca3",
+    "url": "/timeslotdemo/static/css/2.4e28f7e4.chunk.css"
+  },
+  {
+    "revision": "53afe0e340668560c5045233eaaf021e",
+    "url": "/timeslotdemo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/timeslotdemo/precache-manifest.1594043a643ab86ac7ef3f96cc4462b8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/timeslotdemo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
